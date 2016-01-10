34e36dd7a1fd173d4c421227e70588c3fd824ee8
Denis Malyshev <denis.malyshev@teamdev.com>
2016-01-10 21:28:50 +0200

    Added JavaConfig.

---
+++ webapp/src/main/java/com/teamdev/web/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.teamdev.web.config;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import javax.annotation.PostConstruct;
+
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = {"com.teamdev.chat.service.impl", "com.teamdev.chat.persistence"})
+public class ApplicationConfig {
+
+    private static final Logger LOG = Logger.getLogger(ApplicationConfig.class);
+
+    @PostConstruct
+    public void doSomething() {
+        LOG.info("Spring was initialized.");
+    }
+}
---
+++ webapp/src/main/java/com/teamdev/web/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.teamdev.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.teamdev.web.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
