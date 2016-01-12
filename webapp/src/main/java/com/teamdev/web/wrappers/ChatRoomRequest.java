7b97c39c3d7799d80480ce1a74624a7b79e6176d
Denis Malyshev <denis.malyshev@teamdev.com>
2016-01-12 19:40:12 +0200

    Created wrappers for requests.

--- webapp/src/main/java/com/teamdev/web/wrappers/ChatRoomRequest.java
+++ webapp/src/main/java/com/teamdev/web/wrappers/ChatRoomRequest.java
@@ -1,15 +1,17 @@
-package com.teamdev.web.requset;
+package com.teamdev.web.wrappers;
 
 import com.teamdev.chat.service.impl.dto.Token;
 import com.teamdev.chat.service.impl.dto.UserId;
 
-public class TokenRequest {
+public class ChatRoomRequest {
 
     public final Token token;
     public final UserId userId;
+    public final String name;
 
-    public TokenRequest(Token token, UserId userId) {
+    public ChatRoomRequest(Token token, UserId userId, String name) {
         this.token = token;
         this.userId = userId;
+        this.name = name;
     }
 }
