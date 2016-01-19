462585069a2adc417c66ffad194aef0ab6c393dc
Denis Malyshev <denis.malyshev@teamdev.com>
2016-01-19 19:12:38 +0200

    Implemented 2 HQuery (findUsersByChatId and findChatRoomsByUserId).

--- persistence/src/main/java/com/teamdev/chat/persistence/ChatRoomRepository.java
+++ persistence/src/main/java/com/teamdev/chat/persistence/ChatRoomRepository.java
@@ -1,11 +1,19 @@
 package com.teamdev.chat.persistence;
 
 import com.teamdev.chat.persistence.dom.ChatRoom;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface ChatRoomRepository extends CrudRepository<ChatRoom, Long> {
 
     ChatRoom findByName(String name);
+
+    @Query(value = "select * from chatroom c inner join user_chatroom ch on ch.chatRooms_id = c.id and ch.users_id = :id",
+            nativeQuery = true)
+    Set<ChatRoom> findChatRoomsByUserId(@Param("id") long userId);
 }
--- persistence/src/main/java/com/teamdev/chat/persistence/UserRepository.java
+++ persistence/src/main/java/com/teamdev/chat/persistence/UserRepository.java
@@ -1,9 +1,9 @@
 package com.teamdev.chat.persistence;
 
-import com.teamdev.chat.persistence.dom.ChatRoom;
 import com.teamdev.chat.persistence.dom.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
@@ -13,6 +13,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmail(String email);
 
-//    @Query(value = "select chatRooms from user where userId = ?0", nativeQuery = true)
-//    Set<ChatRoom> selectChatRooms(long userId);
+    @Query(value = "select * from user u inner join user_chatroom ch on ch.users_id = u.id and ch.chatRooms_id = :id",
+            nativeQuery = true)
+    Set<User> findUsersByChatRoomId(@Param("id") long chatRoomId);
 }
