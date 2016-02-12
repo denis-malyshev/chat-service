c27b389ea171d9f884fad515d8d46bf3cb952208
Denis Malyshev <denis.malyshev@teamdev.com>
2016-02-12 15:00:42 +0200

    Improved code in EventBus (method unsubscribe renamed to unsubscribeOldConsumers)

--- webapp/src/main/webapp/eventBus.js
+++ webapp/src/main/webapp/eventBus.js
@@ -1,6 +1,15 @@
 function EventBus() {
     this.consumers = [];
 
+    this.unsubscribeOldConsumers = function (type) {
+        for (var j = 0; j < this.consumers.length; j++) {
+            if (this.consumers[j].closure == type) {
+                console.log(new Date().toLocaleTimeString() + ": consumer " + this.consumers[j].type + " removed");
+                this.consumers.splice(j, 1);
+                j--;
+            }
+        }
+    }
 };
 EventBus.prototype.registerConsumer = function (type, consumer, closure) {
     this.consumers.push({
@@ -11,29 +20,16 @@ EventBus.prototype.registerConsumer = function (type, consumer, closure) {
     console.log(new Date().toLocaleTimeString() + ": added consumer " + type);
 };
 EventBus.prototype.postMessage = function (type, message) {
-    var unsubscribe = function (consumers, length, type) {
-        for (var j = 0; j < length; j++) {
-            if (consumers[j].closure == type) {
-                console.log(new Date().toLocaleTimeString() + ": consumer " + consumers[j].type + " removed");
-                consumers.splice(j, 1);
-                j--;
-                length--;
-            }
-        }
-    };
-
-    var consumers = this.consumers;
-    var length = this.consumers.length;
-
-    for (var i = 0; i < length; i++) {
-        if (consumers[i].type == type) {
-            var callback = consumers[i].callback;
+    for (var i = 0; i < this.consumers.length; i++) {
+        if (this.consumers[i].type == type) {
+            var callback = this.consumers[i].callback;
             var myCallback = function () {
                 callback(message);
-                unsubscribe(consumers, length, type);
             };
             setTimeout(myCallback, 100);
+
             console.log(new Date().toLocaleTimeString() + ": message posted to " + type);
         }
     }
+    this.unsubscribeOldConsumers(type);
 };
